eea11b846d482b23f56518f6963b357282d62d69
Pushpak <pushpaknalla1916@gmail.com>
2026-09-22 23:08:02 +0530

    complete javascript_code

---
+++ script.js
@@ -0,0 +1,181 @@
+// Variables --->  Var
+
+var name = "Pushpak"
+console.log(name);
+
+var age = 22
+console.log(age);
+
+var a
+a = 10
+console.log(a);
+
+var b = 20
+b = 40
+console.log(b);
+
+var c = 30
+var c = 50
+console.log(c);
+
+var name ="Pushpak"
+var age = 22
+var city = "Hyderabbad"
+console.log(name, age, city);
+
+var college = "ABC Engineering College"
+console.log(college);
+
+var subject = "Science"
+console.log(subject);
+
+var d = 123
+d = 678
+d = 1987
+d = 39876
+console.log(d);
+
+var salary = 10000
+salary = 30000
+console.log(salary);
+
+
+
+// Variables - let
+
+let username = "Pushpak"
+console.log(username);
+
+let userage = 24
+console.log(userage);
+
+let l
+l = 120
+console.log(l);
+
+let f = 100
+f = 200
+console.log(f);
+
+let Name = "Pushpak"
+let Qualification = "Bachelor of Engineering"
+let Location = "Hyderabbad"
+console.log(Name, Qualification, Location);
+
+let course = "JavaScript"
+
+let marks = 50
+marks = 80
+console.log(marks);
+
+let companyName = "Stackly"
+console.log(companyName);
+
+let experience = 0
+
+let mobileModel = "Moto G Power"
+console.log(mobileModel);
+
+// Variables - Const
+
+const UserName = "Pushpak"
+console.log(UserName);
+
+const DOB = "28/10/2003"
+console.log(DOB);
+
+const company = "Stackly"
+
+const country = "India"
+
+const NAME = "Pushpak"
+const AGE = 24
+const City = "Hyderabbad"
+console.log(NAME, AGE, City);
+
+const courseName = "JavaScript"
+
+const collegeName = "ABC Engineering College"
+console.log(collegeName);
+
+const favoriteColor = "Red"
+console.log(favoriteColor);
+
+const employeeId = "STK-26-000000"
+console.log(employeeId);
+
+const officeLocation = "Vizag"
+console.log(officeLocation);
+
+// Printing Statements
+
+console.log(100);
+
+console.log("Pushpak");
+
+console.log(24);
+console.log("Bachelor of Engineering");
+
+console.log("Welcome to JavaScript");
+
+console.log(9999);
+console.log("ABCD");
+console.log("&^%$");
+console.log("Pushpak_123");
+console.log(1234);
+
+var userName = "Pushpak"
+console.log(userName);
+
+var userAge = 24
+console.log(userAge);
+
+console.log("Pushpak", 24, "Hyderabbad", "Bachelor of Engineering");
+
+
+// Popup Methods
+
+alert("Welcome to JavaScript");
+
+var name = "Pushpak"
+alert(name);
+
+var name = prompt("What is your name?")
+alert(name);
+
+var age = prompt("What is your Age?")
+console.log(age);
+
+let UserQualification =  prompt("What is your Qualification ?")
+alert(UserQualification);
+
+let UserCity = prompt("Where are you from?")
+console.log(UserCity);
+
+confirm("Do you Know JavaScript?")
+
+confirm("Do you want to continue?")
+
+var name = prompt("what is ur name?")
+var qualification =prompt("What is ur qualification?")
+document.writeln(name, qualification);
+
+// document.writeln() with Console Methods
+
+var n = "Hello Everyone"
+document.writeln (n);
+
+console.log("Pushpak");
+console.warn("Bachelor of Engineering");
+console.error("Hyderabbad");
+
+
+// User Info Task
+
+var usersname = prompt("What is your name?");
+var usersage = prompt ("What is your Age?");
+var usersqualification = prompt ("What is your qualification?");
+console.log(usersname);
+console.warn(usersage);
+console.error(usersqualification);
+document.writeln(usersname, usersage, usersqualification);
